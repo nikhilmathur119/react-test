83e6697bfbd7c601e727b99bb8e6c1710ae75fd8
nikhil <nikhil.mathur2022@vitstudent.ac.in>
2024-04-26 19:33:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import axios from'axios';
+import './App.css';
+import React, {useState} from 'react';
+function App() {
+  const [code,setCode]=useState('');
+  const [output,setOutput]=useState("");
+  const handleSubmit =async()=>{
+    const payload={
+      language:"cpp",
+      code
+    };
+    try{
+      const {data}=await axios.post("http://172.17.19.0:5000/run",payload)
+      setOutput(data.output);
+    }
+    catch(err)
+    {
+      console.log(err.response);
+    }
+  }
+  return (
+    <div className="App">
+     <h1>C++ CODE COMPILER:</h1>
+     <textarea rows="20" cols="75" value={code} onChange={(e)=>{setCode(e.target.value)}}>
+     </textarea><br></br>
+     <button onClick={handleSubmit}>Submit</button>
+     <p>{output}</p>
+    </div>
+  );
+}
+
+export default App;
